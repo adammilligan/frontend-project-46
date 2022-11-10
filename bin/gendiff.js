6f43e58629e7bdd57dbf727848291d6bebe724bf
Вадим Валыгин <vadim12388@mail.ru>
2022-11-10 19:13:50 +0300

    add bin src gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,6 @@
+const { Command } = import ('commander');
+const program = new Command();
+program
+    .version('0.0.1', '-V, --version', 'output the version number')
+    .description('Compares two configuration files and shows a difference.')
+program.parse();
