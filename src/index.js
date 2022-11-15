4ba59ebb6c18a1d1d7086370ac9fc9d789743e3e
Вадим Валыгин <vadim12388@mail.ru>
2022-11-15 19:12:17 +0300

    linter makeNode edit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { readFileSync } from 'fs';
 import path from 'node:path';
-import makeNode from "./makeNode.js";
+import makeNode from './makeNode';
 
-const getData = (pathFile) => readFileSync(pathFile, 'utf-8');
-const getTypeFile = (pathFile) => path.extname(pathFile).slice(1);
+// const getData = (pathFile) => readFileSync(pathFile, 'utf-8');
+// const getTypeFile = (pathFile) => path.extname(pathFile).slice(1);
 
 export default (file1, file2) => {
-    const file1Object = JSON.parse(readFileSync(file1, 'utf-8'))
-    const file2Object = JSON.parse(readFileSync(file2, 'utf-8'))
-    console.log(makeNode(file1Object, file2Object))
-}
+  const file1Object = JSON.parse(readFileSync(file1, 'utf-8'));
+  const file2Object = JSON.parse(readFileSync(file2, 'utf-8'));
+  // console.log(makeNode(file1Object, file2Object));
+};
