1fe75a5a787625ec8996da5e6148e2b3d8b36689
Вадим Валыгин <vadim12388@mail.ru>
2022-11-21 18:18:41 +0300

    test yaml edit

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,16 +10,31 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-const formats = ['json', 'ini', 'yml'];
+// const formats = ['json', 'ini', 'yml'];
+//
+// describe('gendiff', () => {
+//   const getResult = (format) => readFile(getFixturePath(format));
+//
+//   test.each(formats)('gendiff %s format', (format) => {
+//     const pathToFile1 = getFixturePath(`before.${format}`);
+//     const pathToFile2 = getFixturePath(`after.${format}`);
+//     expect(genDiff(pathToFile1, pathToFile2)).toEqual(getResult('stylish'));
+//     expect(genDiff(pathToFile1, pathToFile2, 'plain')).toEqual(getResult('plain'));
+//     expect(genDiff(pathToFile1, pathToFile2, 'json')).toEqual(getResult('json'));
+//   });
+// });
 
-describe('gendiff', () => {
-  const getResult = (format) => readFile(getFixturePath(format));
+const beforeFlatJson = getFixturePath('before_flat.json');
+const afterFlatJson = getFixturePath('after_flat.json');
+const beforeFlatYaml = getFixturePath('before_flat.yml');
+const afterFlatYaml = getFixturePath('after_flat.yml');
+const expected = readFile('resultTest.txt');
 
-  test.each(formats)('gendiff %s format', (format) => {
-    const pathToFile1 = getFixturePath(`before.${format}`);
-    const pathToFile2 = getFixturePath(`after.${format}`);
-    expect(genDiff(pathToFile1, pathToFile2)).toEqual(getResult('stylish'));
-    expect(genDiff(pathToFile1, pathToFile2, 'plain')).toEqual(getResult('plain'));
-    expect(genDiff(pathToFile1, pathToFile2, 'json')).toEqual(getResult('json'));
-  });
+test('first test', () => {
+  expect(genDiff(beforeFlatJson, afterFlatJson)).toEqual(expected);
+  expect(genDiff(beforeFlatYaml, afterFlatYaml)).toEqual(expected);
 });
+
+//
+// // console.log(expected);
+// // console.log(genDiff(beforeFlatJson, afterFlatJson));
