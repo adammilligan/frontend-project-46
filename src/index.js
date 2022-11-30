4862cf71fc80efcfe8c9d6acf28bec255a0467c4
Вадим Валыгин <vadim12388@mail.ru>
2022-11-30 12:40:33 +0300

    rename const in index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ const getData = (fullPath) => fs.readFileSync(fullPath, 'utf-8');
 const getTypeFile = (pathFile) => path.extname(pathFile).slice(1);
 
 export default (file1, file2, format = 'stylish') => {
-  const file1Object = parse(getData(file1), getTypeFile(file1));
-  const file2Object = parse(getData(file2), getTypeFile(file2));
+  const dataFile1 = parse(getData(file1), getTypeFile(file1));
+  const dataFile2 = parse(getData(file2), getTypeFile(file2));
 
-  const node = makeTree(file1Object, file2Object);
+  const node = makeTree(dataFile1, dataFile2);
 
   return formatData(node, format);
 };
